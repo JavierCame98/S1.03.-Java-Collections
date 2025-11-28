d2d88b4cb9b58de63c2e6634c4f798ceb05799e3
Javier Castillo <javiercame98@gmail.com>
2025-11-28 17:00:37 +0100

    feat: creating the second list and a ListIterator interface. While loop to show List1 reverse.

--- src/S0312/MainInteger.java
+++ src/S0312/MainInteger.java
@@ -22,7 +22,15 @@ public class MainInteger {
 
         System.out.println("The first List order is: " + integerList1);
 
+        List<Integer> integerList2 = new ArrayList<>();
+        ListIterator<Integer> integerListIterator = integerList1.listIterator(integerList1.size());
 
+        while(integerListIterator.hasPrevious()){
+            int reverseNumber = integerListIterator.previous();
+            integerList2.add(reverseNumber);
+        }
+
+        System.out.println("The second List reverse order is: " + integerList2);
 
 
 
