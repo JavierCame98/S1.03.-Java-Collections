0023bc716da718025ec4a198782db22846bbf5e4
Javier Castillo <javiercame98@gmail.com>
2025-11-28 16:53:34 +0100

    initial commit feat: creating MainInteger class with the first list of integers

---
+++ src/S0312/MainInteger.java
@@ -0,0 +1,35 @@
+package S0312;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainInteger {
+    public static void main(String[] args) {
+
+        List<Integer> integerList1 = new ArrayList<>();
+
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int num4 = 4;
+
+        integerList1.add(num1);
+        integerList1.add(num2);
+        integerList1.add(num3);
+        integerList1.add(num4);
+
+        System.out.println("The first List order is: " + integerList1);
+
+
+
+
+
+
+
+
+
+
+    }
+}
