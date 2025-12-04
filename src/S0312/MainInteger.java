353a4d180de1a07c728a989df3c82f17e0e969de
Javier Castillo <javiercame98@gmail.com>
2025-12-04 12:54:34 +0100

    fix: Optimizing the List

--- src/S0312/MainInteger.java
+++ src/S0312/MainInteger.java
@@ -8,20 +8,9 @@ import java.util.ListIterator;
 public class MainInteger {
     public static void main(String[] args) {
 
-        List<Integer> integerList1 = new ArrayList<>();
-
-        int num1 = 1;
-        int num2 = 2;
-        int num3 = 3;
-        int num4 = 4;
-
-        integerList1.add(num1);
-        integerList1.add(num2);
-        integerList1.add(num3);
-        integerList1.add(num4);
+        List<Integer> integerList1 = new ArrayList<>(List.of(1,2,3,4));
 
         System.out.println("The first List order is: " + integerList1);
-
         List<Integer> integerList2 = new ArrayList<>();
         ListIterator<Integer> integerListIterator = integerList1.listIterator(integerList1.size());
 
